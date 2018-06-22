d587927218f987a71f04e0214808642c96595041
pentamania <tanigami7220@gmail.com>
2018-06-23 01:19:11 +0900

    [update] update performance-test samples

--- performance-test/common.js
+++ performance-test/common.js
@@ -17,8 +17,12 @@ var SKIP = FPS/DRAW_FPS;
 var ASSETS = {
   image: {
     'fox': '../fox64x64.png'
+  },
+  pixi: {
+    'fox': '../fox64x64.png'
   }
 };
+var FONT_SIZE = 16;
 var ADDED_SPRITE = 20;
 var counterDisplay = document.getElementById('counterDisplay');
 
@@ -41,25 +45,27 @@ var generateFox = function(x, y, type, texture) {
     }
   } else {
 
-    // Pixi版
-    fox = new PIXI.Sprite.fromImage(phina.asset.AssetManager.get('image', 'fox').src);
-    // fox = new PIXI.Sprite(texture);
-    fox.position.set(x, y);
-    fox.anchor.set(0.5, 0.5);
-
-    // fox.scale.set(2, 2);
+    /* 純正Pixi版：こちらのほうがちょっと軽い？ */
+    // fox = new PIXI.Sprite.fromImage(phina.asset.AssetManager.get('image', 'fox').src);
+    // fox.position.set(x, y);
+    // fox.anchor.set(0.5, 0.5);
     // fox.scale.set(0.5, 0.5);
-    // fox.scale.set(4, 4);
-    fox.vector = _vector;
+    // fox.angularVelocity = _angVelocity.toRadian();
+
+    /* phina.pixi.PixiSprite版 */
+    fox = phina.pixi.PixiSprite('fox')
+    .setPosition(x, y)
+    .setOrigin(0.5, 0.5);
     fox.angularVelocity = _angVelocity;
 
+    fox.vector = _vector;
     fox.move = function() {
       fox.position.set(fox.x+fox.vector.x, fox.y+fox.vector.y);
-      fox.rotation += (fox.angularVelocity).toRadian();
+      fox.rotation += fox.angularVelocity;
     }
 
+    /* test */
     // fox.alpha = Math.randfloat(0.1, 0.9);
-    // test
     // fox.interactive = true;
     // fox.mousedown = function(){
     //   console.log('dont touch me!')
