accbb5e5c871c6d1be8c6e5b39aebc825e2f7984
pentamania <tanigami7220@gmail.com>
2018-06-22 18:07:32 +0900

    [fix/temp] update Layer class: add method/property to follow phina matrix calculation, override accessor

--- src/Layer.js
+++ src/Layer.js
@@ -12,23 +12,73 @@ import {PixiDisplayElement} from './element.js';
  * @param {Object} options
 */
 export default phina.createClass({
-// phina.define('phina.display.PixiLayer', {
   superClass: PixiDisplayElement,
 
   renderer: null,
 
   init: function(options) {
+    options = ({}).$safe(options, {
+      width: 640,
+      height: 960,
+    });
     this.superInit(null, options);
-
     this.renderer = PIXI.autoDetectRenderer(options.width, options.height, {transparent: true});
+    this._width = options.width;
+    this._height = options.height;
+    this.setOrigin(0, 0); // width/heightに合わせて再セット必要
+
+    // phina側と整合性を取るため必要
+    this._matrix = phina.geom.Matrix33().identity();
+    this._worldMatrix = phina.geom.Matrix33().identity();
 
     this.on('enterframe', function() {
       this.renderer.render(this[PIXI_KEY]);
     });
   },
 
+  // phina側と整合性を取るため必要
+  _calcWorldMatrix: function() {
+    return phina.app.Object2D.prototype._calcWorldMatrix();
+  },
+
   draw: function(canvas) {
-    var domElement = this.renderer.view;
-    canvas.context.drawImage(domElement, 0, 0, domElement.width, domElement.height);
+    // @fixme: こちらではrotationの描画がうまくいかない
+    // var image = this.renderer.view;
+    // canvas.context.drawImage(image,
+    //   0, 0, image.width, image.height,
+    //   -this.width*this.originX, -this.height*this.originY, this.width, this.height
+    // );
+    var image = this.renderer.view;
+    canvas.context.drawImage(image, 0, 0, image.width, image.height);
+  },
+
+  _accessor: {
+    /**
+     * @property    width
+     * 再セットするとoriginに不具合あり
+     */
+    width: {
+      "get": function() {
+        return this._width;
+      },
+      "set": function(v) {
+        this._width = v;
+        // this[PIXI_KEY].width = v;
+        this.renderer.resize(v, this.height);
+      }
+    },
+    /**
+     * @property    height
+     */
+    height: {
+      "get": function() {
+        return this._height;
+      },
+      "set": function(v) {
+        this._height = v;
+        // this[PIXI_KEY].height = v;
+        this.renderer.resize(this.width, v)
+      }
+    },
   },
 });
