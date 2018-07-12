885dd7915ea593943b7eae7d2549ddd037c9b070
pentamania <tanigami7220@gmail.com>
2018-07-13 00:37:58 +0900

    [update] update Sprite class: add frame origin setting feature, change argument order of setImage

--- src/Sprite.js
+++ src/Sprite.js
@@ -8,7 +8,9 @@ import {PixiDisplayElement} from './element.js';
  * @class   PixiSprite
  * @extends PixiDisplayElement
  *
- * @param {string|phina.pixi.PixiTexture|PIXI.Texture} image String: should be a key name
+ * @param {string|phina.pixi.PixiTexture|PIXI.Texture} image - String should be a key name
+ * @param {number} [width]
+ * @param {number} [height]
  */
 export default phina.createClass({
 // export default phina.define('phina.pixi.PixiSprite', {
@@ -18,12 +20,12 @@ export default phina.createClass({
 
   init: function(image, width, height) {
     this.superInit(new PIXI.Sprite());
-    this.setImage(image, 0, 0, width, height);
+    this.setImage(image, width, height);
 
     this._frameIndex = 0;
     this._maxFrameIndex = 0;
-    // this._frameOriginX = 0;
-    // this._frameOriginY = 0;
+    this._frameOriginX = 0;
+    this._frameOriginY = 0;
     this.frameWidth = this.width;
     this.frameHeight = this.height;
     this._cachedFrames = {};
@@ -32,20 +34,20 @@ export default phina.createClass({
   /**
    * setImage
    * @param {string|phina.pixi.PixiTexture|PIXI.Texture} image
-   * @param {number} [x]
-   * @param {number} [y]
    * @param {number} [width]
    * @param {number} [height]
+   * @param {number} [x]
+   * @param {number} [y]
    * @return {this}
    */
-  setImage: function(image, x, y, width, height) {
+  setImage: function(image, width, height, x, y) {
     if (typeof image === 'string') {
       image = phina.asset.AssetManager.get('pixi', image);
       if (!image) throw new Error("[phina-pixi]: Image does not exist in AssetManager")
     }
 
     /* フレーム情報があったら、それに応じて新しいframeを生成する */
-    if (image.createFrame && typeof x === 'number') {
+    if (image.createFrame && arguments[1] != null) {
       image = image.createFrame(x, y, width, height);
     }
     this._image = image;
@@ -71,18 +73,30 @@ export default phina.createClass({
   /**
    * indexに応じたフレームのセット
    * @instance
-   * @param {phina.pixi.PixiTexture|PIXI.Sprite} image
+   * @param {number|object} index - Could be a param object
    * @param {number} [width]
    * @param {number} [height]
+   * @param {number} [x] - origin-x of frame
+   * @param {number} [y] - origin-y of frame
    * @return {this}
    */
-  setFrameIndex: function(index, width, height) {
-    var fw  = this.frameWidth = (width || this.frameWidth);
-    var fh  = this.frameHeight = (height || this.frameHeight);
+  setFrameIndex: function(index, width, height, x, y) {
+    if (typeof index === 'object') {
+      var params = ({}).$extend(index);
+      index = params.index;
+      width = params.width;
+      height = params.height;
+      x = params.x;
+      y = params.y;
+    }
+    var ox = this._frameOriginX = (x || this._frameOriginX);
+    var oy = this._frameOriginY = (y || this._frameOriginY);
+    var fw = this.frameWidth = (width || this.frameWidth);
+    var fh = this.frameHeight = (height || this.frameHeight);
     var _index = (this._maxFrameIndex !== 0) ? index%this._maxFrameIndex : index;
 
     /* check cached frame */
-    var cachedFrame = this._getCachedFrame(_index, fw, fh);
+    var cachedFrame = this._getCachedFrame(_index, fw, fh, ox, oy);
     if (cachedFrame != null) {
       this.setImage(cachedFrame);
       this._frameIndex = _index;
@@ -91,14 +105,16 @@ export default phina.createClass({
 
     /* create new frame */
     var baseTexture = this[PIXI_KEY].texture.baseTexture;
-    var row = ~~(baseTexture.width / fw);
-    var col = ~~(baseTexture.height / fh);
+    var row = ~~((baseTexture.width-ox) / fw);
+    var col = ~~((baseTexture.height-oy) / fh);
     var maxIndex = this._maxFrameIndex = row*col;
     index = index%maxIndex;
 
-    var x = index%row;
-    var y = ~~(index/row);
-    this.setImage(this._image, x*fw, y*fh, fw, fh);
+    var sx = index%row;
+    var sy = ~~(index/row);
+    this.setImage(this._image, fw, fh, sx*fw+ox, sy*fh+oy);
+    this._image._frameOriginX = ox;
+    this._image._frameOriginY = oy;
     this._frameIndex = index;
     // console.log('new frame!');
 
@@ -112,18 +128,26 @@ export default phina.createClass({
   /**
    * cacheしたフレームが
    *  ->ない => return null (make new frame)
-   *  ->ある =>  フレームのwidth, heightが変更されて
+   *  ->ある =>  フレームのwidth, height等が変更されて
    *   -> ない => return _cachedFrames[index]
    *   -> いる => return null (make new frame)
    * @private
    * @param {number} index
    * @param {number} width
    * @param {number} height
+   * @param {number} ox
+   * @param {number} oy
+   * @return {phina.pixi.PixiTexture|null}
    */
-  _getCachedFrame: function(index, width, height) {
+  _getCachedFrame: function(index, width, height, ox, oy) {
     if (this._cachedFrames[index]) {
       var frame = this._cachedFrames[index];
-      if (frame.width === width && frame.height === height) {
+      if (
+        frame.width === width
+        && frame.height === height
+        && frame._frameOriginX === ox
+        && frame._frameOriginY === oy
+      ) {
         return frame;
       } else {
         return null;
